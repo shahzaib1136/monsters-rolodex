05b2ef6b8bf543fcbdbbde3dea8ccf82e7396189
Shahzaib <shahzaib1136@gmail.com>
2020-07-09 22:55:37 +0500

    Updates

---
+++ precache-manifest.c648f4b357a7ec52ca7012969f9b1dc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7660d7e7b28473ec65654987b252fb3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d5433696c2b8d265b63d",
+    "url": "/monsters-rolodex/static/css/main.a282728f.chunk.css"
+  },
+  {
+    "revision": "784b3f07fd9d55949a98",
+    "url": "/monsters-rolodex/static/js/2.493bcd7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.493bcd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5433696c2b8d265b63d",
+    "url": "/monsters-rolodex/static/js/main.b1172ee6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
